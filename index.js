e369d30d476a8e6f9837a0191ae691b314e338b0
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2025-04-06 23:24:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ app.options('*', (req, res) => {
   res.sendStatus(200);
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://tienda-on-line-seven.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
 const PORT = 5000;
 // ⚠️ USANDO dotenv----------------------------------------------------
 const SECRET =  process.env.CLAVESECRET;  
